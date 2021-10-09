296e2018f0849c60d1bbadb2c72816584e9e426f
Abdullah <36342474+sqoor@users.noreply.github.com>
2021-10-09 15:54:09 +0200

    cleaning code

---
+++ argument.py
@@ -0,0 +1,58 @@
+import argparse
+
+""" Documentation
+    Description:
+        Argument is a class with one static method get(), used to read all the arguments from the command line/terminal 
+        and read the values and use it in this script, all the arguments related to connecting to the database.
+        Verbose flag used to print to the console the status of the script (i.e connected to db, view updated,... etc)
+
+        List of argument:
+            --driver='db driver'
+            --server='db server name'
+            --userid='db user'
+            --password='user password'
+            --database='db name'
+            --verbose: if was specified value = True otherwise None
+        or you can use the shortcut
+            -r='db driver'
+            -s='db server name'
+            -u='db user'
+            -p='user password'
+            -d='db name'
+            -v: if was specified value = True otherwise None
+
+        All the argument are required to run this script, except verbose flag.
+
+    Returns:
+      object: has the list of parsed argument from CMD/terminal so it will be used later in this script.
+"""
+
+
+class Argument:
+    @staticmethod
+    def get():
+        parser = argparse.ArgumentParser(description="This a program that reads a text file and find "
+                                                     "10 most frequent words.", exit_on_error=True)
+        parser.add_argument(
+            '-i',
+            '--input',
+            help='Path to the file to read from',
+            required=True
+        )
+        parser.add_argument(
+            '-o',
+            '--output',
+            help='Path to  the file to save result to',
+            required=True
+        )
+        # parser.add_argument(
+        #     '-v',
+        #     '--verbose',
+        #     help='it will print descriptive messages of the script status, if it was provided.',
+        #     nargs='?',
+        #     const=True,
+        #     type=bool
+        # )
+
+        return parser.parse_args()
+
--- main.py
+++ main.py
@@ -1,51 +1,82 @@
 import json
 import re
 from itertools import islice
+from argument import Argument
 
-file = open("Tempest.txt", "r")
-file_str = file.read()
 
-arr_data = file_str.lower().split()
-print('Total words count', len(arr_data))
+def main():
+    args = Argument.get()
 
-dict_string = {}
+    file_string = read_file(args.input)
+    words_array = file_string.lower().split()
 
-for non_cleaned_string in arr_data:
-    words_split_on_special_chars = re.split(r'(?:[^-\'a-zA-Z0-9]|[.]\s)+', non_cleaned_string)
+    words_frequencies = create_words_frequencies(words_array)
+    sorted_words_frequencies = sort_dict(words_frequencies)
+    first_10_words = take_first(10, sorted_words_frequencies)
 
-    for word in words_split_on_special_chars:
-        if word in dict_string and not word == '':
-            dict_string[word] = dict_string[word] + 1
-        else:
-            dict_string[word] = 1
+    print_console(first_10_words)
+    write_file(args.output, first_10_words)
 
-words_dict_sorted = dict(sorted(dict_string.items(), key=lambda item: item[1], reverse=True))
 
-for word in words_dict_sorted:
-    # you, to, and
-    # to:   [to, stripe symbols from start and end of the words
-    # and:  and's - How fares the king and's followers?
-    # you:  you'ld - You'ld be king o' the isle, sirrah?
-    # you,-- if you remove lines 12-19
-    # graces!--and
-    # so!-- and
-    # and,--do
-    if 'to' in word: # and output[word] == 1
-        print(word, f'\t\t({words_dict_sorted[word]})')
-        print('______________________________')
+def read_file(path):
+    """
+        path: str
+            The path to a file contains a string to be read
+        Return str
+            The string read from the file
+    """
+    file = open(path, "r")
+    return file.read()
+
+
+def create_words_frequencies(str_array):
+    words_frequencies = {}
+
+    for non_cleaned_string in str_array:
+        words_split_on_special_chars = clean_string(non_cleaned_string)
+
+        for word in words_split_on_special_chars:
+            if word in words_frequencies and not word == '':
+                words_frequencies[word] = words_frequencies[word] + 1
+            else:
+                words_frequencies[word] = 1
+
+    return words_frequencies
+
+
+def clean_string(string):
+    return re.split(r'(?:[^-\'a-zA-Z0-9]|[.]\s)+', string)
+
+
+def sort_dict(words_frequencies):
+    sorted_tuple = sorted(words_frequencies.items(), key=lambda item: item[1], reverse=True)
+    return dict(sorted_tuple)
+
+
+def take_first(n_words, words_frequencies):
+    return words_frequencies if len(words_frequencies) <= n_words else take(n_words, words_frequencies.items())
 
 
 def take(n, iterable):
-    "Return first n items of the iterable as a dict"
+    """Return first n items of the iterable as a dict"""
     return dict(islice(iterable, n))
 
 
-if len(words_dict_sorted) <= 10:
-    print(words_dict_sorted)
-else:
-    first_10_items = take(10, words_dict_sorted.items())
-    for word in first_10_items:
-        print(word, f'({words_dict_sorted[word]})')
+def print_console(words_dict):
+    for word in words_dict:
+        print(word, f'({words_dict[word]})')
 
-# with open('output.txt', 'w') as file:
-#     file.write(json.dumps(output))
+
+def write_file(output_path, words_dict):
+    with open(output_path, 'w') as file:
+        for word in words_dict:
+            file.write(f'{word} ({words_dict[word]})\n')
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
